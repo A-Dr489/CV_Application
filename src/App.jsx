b22b3dcee0be0694e4fb040f98a3f60227a5b455
Asan Sabir <asansaber123321@gmail.com>
2025-05-10 22:39:45 +0300

    just made a shape, and a place where the forms will go, and made the light hover better

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
 import { CoolHover } from './coolHover.jsx'
+import { BasicForm } from './basicForm.jsx';
 import './App.css'
 
 function App() {
   const [position, setPosition] = useState({x: 0, y: 0});
+  
 
   return (
     <>
-      <div className='daddyApp' onPointerMove={e => setPosition({x: e.clientX, y: e.clientY})}>
+      <div className='daddyApp' onPointerMove={e => setPosition({x: e.pageX, y: e.pageY})}>
         <CoolHover position={position}/>
+        <h1 className='welcome'>Welcome to Immortal CV maker</h1>
+        <div className='shape'></div>
+        <div style={{display: "flex", justifyContent: "center"}}>
+          <div className='formDiv'>
+              <BasicForm />
+          </div>
+        </div>
       </div>
     </>
   )
---
+++ src/basicForm.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+export function BasicForm() {
+
+    return(
+     <>
+        <form className='daddy-form'>
+            <div style={{borderBottom: "3px solid white", width: "90%"}}>
+                <h1 style={{fontSize: 3 + "em", textAlign: "left", lineHeight: 1, marginTop: "50px"}}>Personal Details</h1>
+            </div>
+            
+        </form>   
+     </>
+    )
+}
